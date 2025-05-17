13c7264f7ef23441469b29967a66a74ea4eca6a3
LuiggiMC2112 <u20202559@utp.edu.pe>
2025-05-16 21:23:34 -0500

    v2

--- src/paginas/Medios.jsx
+++ src/paginas/Medios.jsx
@@ -4,33 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Medios = () => {
   return (
     <div className="login-card">
-      <h4 className="text-center mb-4">MEDIOS DE PAGO</h4>
-      <div className="terms-content" style={{ textAlign: "justify", fontSize: "0.95rem" }}>
-      <div
-        className="terms-content"
-        style={{ textAlign: 'justify', fontSize: '0.95rem' }}
-      >
-        <div className="row">
-          <div className="col-md-6 text-center">
-            <img
-              src="/visa.png"
-              alt="Logo Visa"
-              className="img-fluid"
-              width={200}
-              height={150}
-            />
-          </div>
-          <div className="col-md-6 text-center">
-            <img
-              src="/PayPal_Logo2014.png"
-              alt="Logo PayPal"
-              className="img-fluid"
-              width={200}
-              height={150}
-            />
-          </div>
+      <div style={{ fontSize: "0.95rem" }}>
+        <div className="d-flex justify-content-center">
+          <img
+            src="/Mediospago.png"
+            alt="Logo Visa"
+            className="img-fluid"
+            style={{ maxWidth: "600px", width: "100%", height: "auto" }}
+          />
         </div>
-      </div> 
       </div>
     </div>
   );
